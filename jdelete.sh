72a3e4e8aaccdda0a500ff4dc386a93a63dd0408
Evan Callicoat <diopter@gmail.com>
2013-12-02 23:30:58 -0600

    Rename some files, ignore pub keys

--- jdelete.sh
+++ jdelete.sh
@@ -4,4 +4,4 @@ cd /opt/virtlab
 source .venv/bin/activate
 export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=1
-ansible-playbook jenkins.yml build.yml $@ 2>&1
+ansible-playbook delete.yml $@ 2>&1
